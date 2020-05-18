6a69bf1608e80b8ac18108d04bb11e8016eff950
Raja Bhowmick <Raja_Bhowmick@infosys.com>
2020-05-19 00:23:50 +1000

    Added build files

---
+++ precache-manifest.3abdc4a98baf13e53c6ec94fe44115a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda1da3cdb44a45ec92d3a0e093a9a2c",
+    "url": "/avro-data-converter/index.html"
+  },
+  {
+    "revision": "7557fc488897757fcca4",
+    "url": "/avro-data-converter/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "20cb30bf460e90ce1e7e",
+    "url": "/avro-data-converter/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7557fc488897757fcca4",
+    "url": "/avro-data-converter/static/js/2.ea9d0d87.chunk.js"
+  },
+  {
+    "revision": "4cd125143c9d6e2ac1485046892b1cbf",
+    "url": "/avro-data-converter/static/js/2.ea9d0d87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20cb30bf460e90ce1e7e",
+    "url": "/avro-data-converter/static/js/main.e00441db.chunk.js"
+  },
+  {
+    "revision": "5177fb6b819591f59b5d",
+    "url": "/avro-data-converter/static/js/runtime-main.c5420f39.js"
+  }
+]);
